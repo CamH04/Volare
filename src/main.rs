9c160183e5e81ece1a429d8bc5a57d1ab6081f44
CamH04 <cameronahaynes04@gmail.com>
2024-07-09 13:54:39 +0100

    tokenized everything other than multi digit numbers

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ use std::fs;
 mod tokenizer;
 
 
+//58:05
+//TODO : turen tokens into assembly now
+
 fn main() {
-    let readfile = fs::read_to_string("test.v").expect("Didnt read");
-    tokenizer::tokenize(&readfile);
-    println!(" ");
+//tokenize code
+    let readfile = fs::read_to_string("test.volare").expect("Didnt read");
+    let returntokens = tokenizer::tokenize(&readfile);
+    println!("tokenize finished:");
+    println!("{:#?}", returntokens);
+
 }
 
 
